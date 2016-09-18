798518dc9611ad7a5aaf9e482ab8f47dc21cf27f
ApurvaDubey <apurva.dubey@gmail.com>
2016-09-18 13:03:43 +0530

    Add files via upload

---
+++ Week2_Exercise3.jl
@@ -0,0 +1,34 @@
+using JuMP
+using Clp
+using DataFrames
+
+#using Gurobi
+m = Model(solver=ClpSolver())
+
+# decision variables
+S = ["x11","x12","x21","x22","x31","x32"]
+W = [1,2,1.2,3,2,3]
+# non-negativity
+@defVar(m, x[S] >= 0)
+
+# read inputs
+# myData = readtable("./fooddata.csv",header=true)
+
+# add constraints
+@constraint(m, x["x11"] + x["x21"] + x["x31"] <= 24)
+@constraint(m, x["x12"] + x["x22"] + x["x32"] <= 24)
+@constraint(m, x["x11"] + 2*x["x12"] <= 15)
+@constraint(m, 1.2*x["x21"] + 3*x["x22"] <= 24)
+@constraint(m, 2*x["x31"] + 3*x["x32"] <= 18)
+
+# Objective function:mimimize calories
+@objective(m, Max,sum([ W[i]*x[j[1]] for (i,j) in enumerate(zip(S,W))]))
+
+# solve the optimization problem
+solve(m)
+
+# determine consumption amounts
+println("variable values: ", getvalue(x))
+
+# determine optimal cost of consumption
+println("Objective value: ", getobjectivevalue(m))
